ea1220be2054c6fcdc0dd5fcd507c54e6c704fe7
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-04-04 13:18:06 +0400

    add: messages when guessing the number

--- script.js
+++ script.js
@@ -10,5 +10,21 @@ function play() {
   var user_guess = document.getElementById("guess").value;
   if (user_guess < 1 || user_guess > 100) {
     alert("Please Enter a number between 1 and 100");
+  } else {
+    guessed_nums.push(user_guess);
+    no_of_guesses += 1;
+    if (user_guess < answer) {
+      msg1.textContent = "Your Guess is too low.";
+      msg2.textContent = "No. of guesses: " + no_of_guesses;
+      msg3.textContent = "Guessed numbers are: " + guessed_nums;
+    } else if (user_guess > answer) {
+      msg1.textContent = "Your Guess is too high.";
+      msg2.textContent = "No. of guesses: " + no_of_guesses;
+      msg3.textContent = "Guessed numbers are: " + guessed_nums;
+    } else if (user_guess == answer) {
+      msg1.textContent = "Yippie You win!!";
+      msg2.textContent = "The number was: " + answer;
+      msg3.textContent = "You guessed it in " + no_of_guesses + " guesses";
+    }
   }
 }
